d755d8678330813a534bda3063c34948c42fc046
HannesPrinsloo <johannespprinsloo@gmail.com>
2025-04-27 00:08:22 +0200

    It works! All tests passed. I would like to handle keypresses and do some extra aesthetics, but it's good enough for now

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 
+const operatorsRegEx = /[+\-*/]/;
+const decimalRegEx = /^-?(?:\d+|\d+\.\d*)$/;
+const leadingZeroRegEx = /^(?:[1-9]|0\.)/g;
+
+
+const workingWithNumbers = (input) => {
+  const evaluated = eval(input.join(''));
+  console.log("workingWithNumbers - evaluated -->", evaluated);
+  console.log("evaluated LENGTH", evaluated.toString().length);
+}
 
 const Main = () => {
   const [input, setInput] = useState("");
   const [result, setResult] = useState('');
   const [tempNum, setTempNum] = useState([]);
-  // const [display, setDisplay] = useState([input, result]);
 
-  const operatorsRegEx = /[+\-*/]/;
-  const decimalRegEx = /^-?(?:\d+|\d+\.\d*)$/;
-  const leadingZeroRegEx = /^(?:[1-9]|0\.)/g;
-  // const tempNum = [];
+  
+ 
   
   const handleClick = (event) => {
     console.log("event.target.value ->", event.target.value);
@@ -25,55 +32,112 @@ const Main = () => {
     const lastInputIndex = (input.length -1);
     console.log(lastInputIndex, "🔔 lastInputIndex")
 
-
+    //======================================== EQUALS ========================================
     if (value === "=") {
-      console.log("Equals", value);
-    } else if (value === "AC") {
-      console.log("All Clear", value);
-    } else if (value === "DEL") {
-      console.log("Delete", value);
-    } else if ((value.match(operatorsRegEx) && input.length > 0) || (value === "-" && input.length === 0)) {
-      if (lastInputElement === "-" && value.match(operatorsRegEx) && input.length === 1) {
-        console.log("FFS")
+      
+      console.log("Equals 🔥🔥", value);
+      if (input.length === 0) {
         return;
       }
-      if (lastInputElement.match(operatorsRegEx) && input.length > 0) {
-        if((input.length > 0)) {
-          setInput([
-            ...input.slice(0, lastInputIndex), value
-          ]
-          );
-        } else if (input.length === 0 && value === "-") {
+      if (lastInputElement === "=") {
+        return;
+      }
+      if (lastInputElement.match(operatorsRegEx)) {
+        console.log("Operator before equal");
+        return;
+      }
+      const finalResult = Math.floor(eval(input.join('')) * 1000000) / 1000000;
+      setResult(finalResult);
+      setInput([...input, value]);
+      console.log("Equals after input", finalResult);
+
+      setTempNum([]);
+      
+    }// ======================================== ALL CLEAR ========================================
+      else if (value === "AC") {
+        setResult('');
+        setInput('');
+        setTempNum([]);
+        console.log("All Clear", value);
+    }// ======================================== DELETE ========================================
+      else if (value === "DEL") {
+        console.log("Delete", value);
+        setInput([...input.slice(0, lastInputIndex)]);
+    }//======================================== OPERATORS ========================================
+      else if ((value.match(operatorsRegEx) && input.length > 0) || (value === "-" && input.length === 0)) {
+        if (lastInputElement === "-" && value.match(operatorsRegEx) && input.length === 1) {
+          console.log("FFS")
           return;
         }
-        return;
-      }
-      console.log("Operator", value);
-      setInput([...input, value]);
-      setTempNum([]);
-    } else if (value.match(/\d|\./)) {
-      console.log("Digit or decimal", value);
-      setTempNum([...tempNum, value]);
-      
-      const tempNumCheck = letTempNum.join('').match(decimalRegEx);
-      const leadingZeroCheck = letTempNum.join('').match(leadingZeroRegEx);
-      console.log("00 check", leadingZeroCheck);
-      
-      if (!tempNumCheck || (letTempNum.length > 1 && !leadingZeroCheck)) {
-        console.log("tempNumCheck ->", tempNumCheck);
-        console.log("leadingZeroCheck ->", leadingZeroCheck)
-        console.log("letTempNum === falsy ---> tempNum:", letTempNum);
-        letTempNum.splice(lastElementIndex, 1);
-        setTempNum(letTempNum);
-        console.log("letTempNum after splice ->", letTempNum);
-        setInput([...input]);
-      } else {
-        console.log("tempNumCheck ->", tempNumCheck)
-        console.log("letTempNum === truthy ---> tempNum:", letTempNum);
+        const secondLastInputElement = `${input[(input.length -2)]}`;
+        console.log("secondLastInputElement ------->", secondLastInputElement);
+        if (secondLastInputElement.match(operatorsRegEx) && lastInputElement === "-" && value.match(operatorsRegEx)) {
+          setInput([...input.slice(0, -2), value]);
+          console.log("Slice off two🐲🐉");
+          return;
+        };
+        if (lastInputElement.match(operatorsRegEx) && value === "-") {
+          console.log("Operator minus issue")
+          if (lastInputElement.match(operatorsRegEx) && value === "-") {
+            setInput([...input, value]);
+            console.log("POES!");
+            return;
+          };
+         
+          setInput([...input.slice(0, lastInputIndex), value]);
+          return;
+        }
+        if (lastInputElement === "=") {
+          const removeEquals = [...input.slice(0, lastInputIndex)]
+          console.log("removeEquals ----->", removeEquals)
+          const finalResult = Math.floor(eval(removeEquals.join('')) * 1000000) / 1000000;
+          console.log("finalResult after removeEquals ===>", finalResult);
+          setInput([finalResult, value]);
+          return;
+        };
+        
+        if (lastInputElement.match(operatorsRegEx) && input.length > 0) {
+          if((input.length > 0)) {
+            setInput([
+              ...input.slice(0, lastInputIndex), value
+            ]
+            );
+          } else if (input.length === 0 && value === "-") {
+            return;
+          }
+          return;
+        }
+        console.log("Operator", value);
         setInput([...input, value]);
-      }
-      
-    } 
+        setTempNum([]);
+    }//======================================== NUMBERS & DECIMAL ========================================
+      else if (value.match(/\d|\./)) {
+        console.log("Digit or decimal", value);
+        setTempNum([...tempNum, value]);
+        if (lastInputElement === "=") {
+          setInput([value]);
+          setResult('');
+          return;
+        }
+        
+        const tempNumCheck = letTempNum.join('').match(decimalRegEx);
+        const leadingZeroCheck = letTempNum.join('').match(leadingZeroRegEx);
+        console.log("00 check", leadingZeroCheck);
+        
+        if (!tempNumCheck || (letTempNum.length > 1 && !leadingZeroCheck)) {
+          console.log("tempNumCheck ->", tempNumCheck);
+          console.log("leadingZeroCheck ->", leadingZeroCheck)
+          console.log("letTempNum === falsy ---> tempNum:", letTempNum);
+          letTempNum.splice(lastElementIndex, 1);
+          setTempNum(letTempNum);
+          console.log("letTempNum after splice ->", letTempNum);
+          setInput([...input]);
+        } else {
+          console.log("tempNumCheck ->", tempNumCheck)
+          console.log("letTempNum === truthy ---> tempNum:", letTempNum);
+          setInput([...input, value]);
+        }
+      } 
 
   };
 
@@ -94,10 +158,15 @@ const Buttons = ({ handleClick, input, result }) => {
 
   const Display = () => {
     return (
-      <div id="display" className="Display d-flex justify-content-center">
-        <div className="row justify-content-sm-center">
-          <p className="col col-sm-12 btn">{input}</p>
-          <p className="col col-sm-12 btn">{result}</p>
+      <div  className="Display d-flex justify-content-center">
+        <div  className="row justify-content-sm-center">
+          <p className="col col-sm-12 btn">{input.length === 0 ? "0" : input}</p>
+          <p id="display" className="col col-sm-12 btn">
+            {result.length === 0 && input.length === 0 ? 0 
+            : result.length === 0 && input.length > 0 ? input.join('').toString()
+            : result.length > 0 ? result.toString()
+            : result.toString()}
+            </p>
         </div>
       </div>
     )
